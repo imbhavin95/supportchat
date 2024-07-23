b62db575e21b1dc71f0a9ee967bf34285bb70074
Bhavin Sasapra <imbhavin95@gmail.com>
2024-07-23 17:07:21 +0530

    initial push

---
+++ config.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * ==========================================================
+ * INITIAL CONFIGURATION FILE
+ * ==========================================================
+ *
+ * Insert here the information for the database connection and for other core settings.
+ *
+ */
+
+/* Plugin folder url */
+define('SB_URL', 'https://supportboard.test');
+
+/* The name of the database */
+define('SB_DB_NAME', 'support_board');
+
+/* MySQL database username */
+define('SB_DB_USER', 'root');
+
+/* MySQL database password */
+define('SB_DB_PASSWORD', 'password');
+
+/* MySQL hostname */
+define('SB_DB_HOST', '127.0.0.1');
+
+/* MySQL port (optional) */
+define('SB_DB_PORT', '');
+define('SB_CROSS_DOMAIN', true);
+define('SB_DIALOGFLOW', true);
+// define('SB_CLOUD', true);
+// define('SB_CLOUD_PATH', 'include/functions.php')
+
+/* [extra] */
+
+?>
